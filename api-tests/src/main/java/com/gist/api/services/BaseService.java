8a3e9aff8ec7aaedad64513030efa37df70621f3
AnatoliiHanziuk <anatoliy.ganzyuk@gmail.com>
2018-10-15 20:04:18 +0200

    Move url and token to property file

--- api-tests/src/main/java/com/gist/api/services/BaseService.java
+++ api-tests/src/main/java/com/gist/api/services/BaseService.java
@@ -1,21 +1,22 @@
 package com.gist.api.services;
 
-import com.gist.api.utils.PropertyUtils;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
+import java.util.ResourceBundle;
+
 import static io.restassured.RestAssured.given;
 
 public class BaseService {
 
-    private final String GIST_BASE_URI = "https://api.github.com/gists";
-    //private final String OAUTH_TOKEN = PropertyUtils.get("token");
-    private final String OAUTH_TOKEN = "903579e3d13cf9108090d6fcbaa1371f7a470f71";
+    private static ResourceBundle rb = ResourceBundle.getBundle("config");
 
     public RequestSpecification setup() {
+        final String GIST_BASE_URI = rb.getString("url");
+        final String OAUTH_TOKEN = rb.getString("token");
         return given()
                 .contentType(ContentType.JSON)
                 .auth().oauth2(OAUTH_TOKEN)
